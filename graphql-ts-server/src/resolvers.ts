19ae823dff123c7269366b1f77608584a8fbc2af
pdvonzoo <a01088570194@gmail.com>
2019-06-05 14:39:39 +0900

    Feat: 유저 등록하는 register 로직 추가

--- graphql-ts-server/src/resolvers.ts
+++ graphql-ts-server/src/resolvers.ts
@@ -1,5 +1,23 @@
-export const resolvers = {
+import * as bcrypt from "bcryptjs";
+import { ResolverMap } from "./types/graphql-utill";
+import { User } from "./entity/User";
+
+export const resolvers: ResolverMap = {
   Query: {
-    hello: (_: any, { name }: any) => `Bye ${name || "World"}`
+    hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || "World"}`
+  },
+  Mutation: {
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashPassword
+      });
+      await user.save();
+      return true;
+    }
   }
 };
