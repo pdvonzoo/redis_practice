760ae97efa3fd0d088a67064d644be94247a7516
pdvonzoo <a01088570194@gmail.com>
2019-06-05 16:17:53 +0900

    Fix: app.address 오류

---
+++ graphql-ts-server/src/modules/register/resolvers.ts
@@ -0,0 +1,20 @@
+import * as bcrypt from "bcryptjs";
+import { ResolverMap } from "../../types/graphql-utill";
+import { User } from "../../entity/User";
+
+export const resolvers: ResolverMap = {
+  Mutation: {
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashPassword
+      });
+      await user.save();
+      return true;
+    }
+  }
+};
