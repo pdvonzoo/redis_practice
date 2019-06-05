9e43749d1251f791dbee10eeba33f3951c94ca8c
pdvonzoo <a01088570194@gmail.com>
2019-06-05 15:41:38 +0900

    Feat: test(jest) 추가 및 develeopement, test에 따른 로직 분리

---
+++ graphql-ts-server/src/tests/register.test.ts
@@ -0,0 +1,31 @@
+import { request } from "graphql-request";
+
+import { host } from "./constants";
+import { User } from "../entity/User";
+import { createTypeormConn } from "../utils/createTypeconn";
+
+beforeAll(async () => {
+  await createTypeormConn();
+});
+
+const email = "tom@bob.com";
+const password = "password";
+
+const mutation = `
+mutation {
+  register(email: "${email}", password: "${password}")
+}
+`;
+test("Register user", async () => {
+  const response = await request(host, mutation);
+  expect(response).toEqual({ register: true });
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
+});
+
+// use a test database
+// drop all data once the test is over
+// when I run yarn test it also start server
